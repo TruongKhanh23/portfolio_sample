cf242ceb6abfffe64bbb93ddc24ec47ba2b2fe66
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-07-05 20:56:27 +0700

    TNK-PS-003: Configure Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,3 @@
-<script>
-import AppBanner from '@/components/shared/AppBanner';
-import ProjectsGrid from '../components/projects/ProjectsGrid.vue';
-import Button from '../components/reusable/Button.vue';
-
-export default {
-	name: 'Home',
-	components: {
-		AppBanner,
-		ProjectsGrid,
-		Button,
-	},
-};
-</script>
-
 <template>
 	<div class="container mx-auto">
 		<!-- Banner -->
@@ -33,5 +18,16 @@ export default {
 		</div>
 	</div>
 </template>
+
+<script>
+import AppBanner from '@/components/shared/AppBanner';
+import ProjectsGrid from '@/components/projects/ProjectsGrid.vue';
+import Button from '@/components/reusable/Button.vue';
+
+export default {
+	name: 'Home',
+	components: { AppBanner, ProjectsGrid, Button },
+};
+</script>
 
 <style scoped></style>
