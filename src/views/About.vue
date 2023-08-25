c5f482e0c7909792c1149556180475757ddc36cb
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-08-25 15:22:24 +0700

    fix: remove children component in About for test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,33 +1,9 @@
 <template>
   <div>
-    <!-- About me -->
     <div class="container mx-auto">
-      <AboutMe />
-    </div>
-
-    <!-- About counter -->
-    <AboutCounter />
-
-    <!-- About counter -->
-    <div class="container mx-auto">
-      <AboutClients />
+      This is page of About
     </div>
   </div>
 </template>
-
-<script setup>
-import AboutMe from '@/components/about/AboutMe'
-import AboutCounter from '@/components/about/AboutCounter'
-import AboutClients from '@/components/about/AboutClients'
-import feather from 'feather-icons'
-import { onMounted, onUpdated } from 'vue'
-
-onMounted(() => {
-  feather.replace()
-})
-onUpdated(() => {
-  feather.replace()
-})
-</script>
 
 <style scoped></style>
