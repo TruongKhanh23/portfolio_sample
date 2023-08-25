a607eacd8b414103be385cbb4e6d3932920549e4
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-08-25 15:27:11 +0700

    fix: import component About Me for testing

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <div class="container mx-auto">This is page of About</div>
     <div class="container mx-auto">
-      This is page of About
+      <AboutMe />
     </div>
   </div>
 </template>
+<script setup>
+import AboutMe from "@/components/about/AboutMe";
 
+</script>
 <style scoped></style>
