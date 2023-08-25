72a53a63120e35dfa67406895ae5e11a81b96b39
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-08-25 15:31:01 +0700

    fix: import component AboutCounter for testing

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,10 +4,13 @@
     <div class="container mx-auto">
       <AboutMe />
     </div>
+    <!-- About counter -->
+		<AboutCounter />
   </div>
 </template>
 <script setup>
 import AboutMe from "@/components/about/AboutMe";
+import AboutCounter from '@/components/about/AboutCounter';
 
 </script>
 <style scoped></style>
