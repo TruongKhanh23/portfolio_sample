5b25619748ca507a28d936898851a9f16d8de236
Truong <truongnguyenkhanh230800@gmail.com>
2023-09-04 13:24:45 +0700

    fix: update const for file name

--- src/components/shared/AppBanner.vue
+++ src/components/shared/AppBanner.vue
@@ -16,8 +16,8 @@
       </p>
       <div class="flex justify-center sm:block">
         <a
-          href="/files/Stoman-Resume.pdf"
-          download="Stoman-Resume.pdf"
+          :href="`/files/${cvFileName}`"
+          :download="cvFileName"
           target="_blank"
           class="flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 dark:border-ternary-dark py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
           aria-label="Download Resume"
@@ -50,7 +50,7 @@ import feather from "feather-icons";
 import { ref, onBeforeMount, onMounted, onUpdated } from "vue";
 
 const theme = ref(null);
-// const cvFilePath = "Lengchhinghor_CV_9th.pdf";
+const cvFileName = "Stoman-Resume.pdf";
 
 // onBeforeMount = created
 onBeforeMount(() => {
