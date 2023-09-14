4b1885cdd1b42df26cf5e6aa4bd013588c72fa4b
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-09-14 17:24:16 +0700

    feat: trial integrate PWA

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,35 @@
-import path from "path"
-import { defineConfig } from "vite"
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      manifest: {
+        name: 'Your App Name',
+        short_name: 'App Name',
+        description: 'Your app description',
+        theme_color: '#4DBA87',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        scope: '/',
+        icons: [
+          {
+            src: '/path/to/your/icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        skipWaiting: true,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@/": `${path.resolve(__dirname, "src")}/`,
     },
   },
-})
+});
