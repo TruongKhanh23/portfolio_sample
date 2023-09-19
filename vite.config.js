1ecb4392809ca20d423484f2c1455dad7bce506a
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2023-09-19 17:37:38 +0700

    chore: update name, icon

--- vite.config.js
+++ vite.config.js
@@ -6,19 +6,24 @@ export default defineConfig({
   plugins: [
     VitePWA({
       manifest: {
-        name: 'K Portfolio',
-        short_name: 'K Portfolio',
-        description: 'Portfolio of Truong Nguyen Khanh',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        scope: '/',
+        name: "K Portfolio",
+        short_name: "K Portfolio",
+        description: "Portfolio of Truong Nguyen Khanh",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        scope: "/",
         icons: [
           {
-            src: '/path/to/your/icon.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "/favicons/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/favicons/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
         ],
       },
