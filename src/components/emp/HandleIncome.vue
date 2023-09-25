f32c1b97cd48ca2932b5b20daa31ebeb319bb3d1
Truong <truongnguyenkhanh230800@gmail.com>
2023-09-25 21:11:40 +0700

    chore: mapping color for each row handle income

--- src/components/emp/HandleIncome.vue
+++ src/components/emp/HandleIncome.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="px-4">
-    <a-table :columns="columns" :data-source="data">
+    <a-table
+      :columns="columns"
+      :data-source="data"
+      :rowClassName="backgroundColor"
+    >
       <template #bodyCell="{ column, text }">
         <template v-if="column.dataIndex === 'amount'">
           <a>{{ new Intl.NumberFormat().format(text) }}</a>
@@ -28,6 +32,23 @@ export default {
       type: Object,
       require: true,
     },
+  },
+  setup() {
+    const typeColor = {
+      necessity: "#ffabab",
+      freedom: "#63b5ff",
+      enjoy: "#76de82",
+      education: "#97a2ff",
+      giving: "#c0c0c0",
+      longTermSaving: "#f9a484",
+    };
+    function backgroundColor(record) {
+      const type = record.type;
+      if (Object.prototype.hasOwnProperty.call(typeColor, type)) {
+        return `bg-[${typeColor[type]}]`;
+      }
+    }
+    return { typeColor, backgroundColor };
   },
 };
 </script>
