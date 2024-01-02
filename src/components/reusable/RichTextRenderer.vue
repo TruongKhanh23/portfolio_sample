4be2bdb9259006dda9ce6360d3e50d2a82358358
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-01-02 14:27:15 +0700

    feat: init RichTextRenderer component

---
+++ src/components/reusable/RichTextRenderer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p v-for="(paragraph, index) in arrayRichText" :key="index">
+      <span v-if="paragraph.nodeType === 'text'">
+        {{ paragraph.value }}
+      </span>
+      <span v-if="paragraph.nodeType === 'hyperlink'">
+        <a
+          :href="paragraph.data.uri"
+          target="_blank"
+          class="text-[blue] underline"
+          >{{ paragraph.data.uri }}</a
+        >
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  arrayRichText: {
+    type: Array,
+    default: () => [],
+  },
+});
+</script>
