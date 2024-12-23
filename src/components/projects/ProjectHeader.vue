bdc4392e0f9e6b01c15d1afe54438a2362cb2900
Trương Khánh <truongn.khanh@dai-ichi-life.com.vn>
2024-12-23 09:44:49 +0700

    P013: Fix UI SingleProject

--- src/components/projects/ProjectHeader.vue
+++ src/components/projects/ProjectHeader.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <!-- Project heading and meta info -->
-    <p
-      class="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-14 sm:mt-20 mb-7 min-h-[20px]"
+    <div
+      class="font-general-medium text-left text-3xl sm:text-4xl font-bold text-primary-dark dark:text-primary-light mt-14 sm:mt-20 mb-4 min-h-[20px]"
     >
-      {{ singleProjectHeader.title }}
-    </p>
-    
-    <div class="flex flex-col min-h-[20px]">
-      <div class="flex items-center mb-4">
+      <p v-if="singleProjectHeader">{{ singleProjectHeader.title }}</p>
+    </div>
+
+    <div class="flex flex-col min-h-[20px] space-y-4">
+      <div v-if="singleProjectHeader.tags" class="flex items-center">
         <i
           data-feather="tag"
           class="w-4 h-4 text-ternary-dark dark:text-ternary-light"
@@ -16,23 +16,24 @@
         <span
           class="font-general-medium ml-2 leading-none text-primary-dark dark:text-primary-light"
         >
-          <template v-for="item, in singleProjectHeader.tags" :key="item">
+          <template v-for="item in singleProjectHeader.tags" :key="item">
             <a-tag>{{ item }}</a-tag>
           </template>
         </span>
       </div>
-      <div class="flex items-center mr-10">
-        <i
-          data-feather="clock"
-          class="w-4 h-4 text-ternary-dark dark:text-ternary-light"
-        ></i>
-        <span
-          class="font-general-medium ml-2 leading-none text-primary-dark dark:text-primary-light"
-        >
-          {{ singleProjectHeader.date }}
-        </span>
+      <div class="flex mr-10 min-h-[5px]">
+        <div v-if="singleProjectHeader.date" class="flex justify-center">
+          <i
+            data-feather="clock"
+            class="w-4 h-4 text-ternary-dark dark:text-ternary-light"
+          ></i>
+          <span
+            class="font-general-medium ml-2 leading-none text-primary-dark dark:text-primary-light"
+          >
+            {{ singleProjectHeader.date }}
+          </span>
+        </div>
       </div>
-      
     </div>
   </div>
 </template>
